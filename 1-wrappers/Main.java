2bb0d030642d7934de435362561e8015d306001a
TheoJustino <theowilliamjustino@gmail.com>
2026-06-06 21:57:47 -0400

    Exercício 1

---
+++ 1-wrappers/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Exercício 1
+        int nota = 8;
+        Integer notaobj = nota;
+        System.out.println(nota); 
+        System.out.println(notaobj); 
+        System.out.println(notaobj.getClass().getSimpleName());
+    }
+}
